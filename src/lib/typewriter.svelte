789b384cb9a6923c73c9d7e76294a3a56e105169
Steven Adams <steven.adams.r@gmail.com>
2024-05-12 09:48:20 -0400

    Initial Commit

---
+++ src/lib/typewriter.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import { tweened } from 'svelte/motion';
+
+	const durationPerCharacter = 100;
+
+	const interval = 2500;
+
+	let index = 0;
+	let timeout: number;
+
+	let threshold: number = 0;
+
+	let words: string[] = [
+		'Fullstack Developer',
+		'Avid Runner',
+		'App Developer',
+		'Emo Kid',
+		'CSS Guru',
+		'Jack of All Trades',
+		'Master of None',
+		'Game Developer',
+		'Amateur Drummer',
+		'NodeJS Expert',
+		'Coffee Addict',
+		'Smelly Nerd',
+		'Wordpress Expert',
+		'Little Tired',
+		'Lovable Geek',
+		'Drupal Know It All',
+		'Level 15 Barbarian',
+		'Introvert',
+		'ISTP-T',
+		'College Dropout'
+	];
+
+	const tween = tweened('', {
+		interpolate: (a: string, b: string) => (t: number) => {
+			let string = '';
+			if (t < threshold) {
+				string = a.substring(0, a.length * (1 - t / threshold));
+			} else {
+				t -= threshold;
+				string = b.substring(0, b.length * (t / (1 - threshold)));
+			}
+
+			return string;
+		}
+	});
+
+	function change() {
+		const next = words[index++ % words.length];
+
+		const totalCharacters = $tween.length + next.length;
+
+		threshold = $tween.length / totalCharacters;
+
+		const spaces = (next.match(' ') || []).length + ($tween.match(' ') || []).length;
+
+		const duration = ($tween.length + next.length - spaces) * durationPerCharacter;
+
+		tween.set(next, {
+			duration
+		});
+
+		timeout = setTimeout(change, duration + interval);
+	}
+
+	onMount(() => {
+		change();
+	});
+
+	onDestroy(() => {
+		clearTimeout(timeout);
+	});
+</script>
+
+<pre class="inline font-sans whitespace-pre-wrap">A {$tween}</pre>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require('tailwindcss/plugin');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			textShadow: {
+				DEFAULT: '0px 0px 20px var(--tw-shadow-color)'
+			},
+			boxShadow: {
+				glow: '0px 0px 20px 0px var(--tw-shadow-color)'
+			}
+		}
+	},
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value
+					})
+				},
+				{ values: theme('textShadow') }
+			);
+		})
+	]
+};
