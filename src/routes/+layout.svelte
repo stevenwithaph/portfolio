789bb4453d425eb8c068df9c1b37d381d1c38f8c
Steven Adams <steven.adams.r@gmail.com>
2024-07-19 10:41:39 -0400

    Mobile borders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-	class="p-7 bg-neutral-800 relative before:fixed before:top-0 before:w-full before:h-7 before:bg-neutral-800 before:z-10 after:fixed after:bottom-0 after:w-full after:h-7 after:bg-neutral-800 after:z-10"
+	class="pt-7 md:pb-7 md:px-7 bg-neutral-800 relative before:fixed before:top-0 before:w-full before:h-7 before:bg-neutral-800 before:z-10 md:after:fixed md:after:bottom-0 md:after:w-full md:after:h-7 md:after:bg-neutral-800 md:after:z-10"
 >
 	<ul class="fixed top-0 right-8 z-10 flex justify-end gap-x-4 text-neutral-400 text-lg">
 		<li>
