93163d5b7afc664090b33b47c51aa265666973ff
Steven Adams <steven.adams.r@gmail.com>
2024-07-27 11:21:34 -0400

    Bolded highlighted text

--- src/lib/highlight.svelte
+++ src/lib/highlight.svelte
@@ -1,5 +1,5 @@
 <span
-	class="bg-gradient-to-r from-green-500 to-green-700 text-transparent bg-clip-text text-shadow shadow-green-500"
+	class="bg-gradient-to-r from-green-500 to-green-700 text-transparent bg-clip-text text-shadow shadow-green-500 font-extrabold"
 >
 	<slot />
 </span>
